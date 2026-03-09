59bbf51384a523e4de64b9376267d15c78c79901
MarkusYESSIR <markus5338soerensen@gmail.com>
2026-03-09 12:23:17 +0100

    Set up a very primitive sensor reading of DHT11. It shows temp and hummidity as JSON and communicates it to server via NodeJS

---
+++ arduino-component/sketch_test/sketch_test.ino
@@ -0,0 +1,49 @@
+#include "DHT.h"
+
+// Define the pin the DHT11 is connected to (Digital Pin 2)
+#define DHTPIN 2
+
+// Define the type of DHT sensor we are using
+#define DHTTYPE DHT11
+
+// Initialize the DHT sensor
+DHT dht(DHTPIN, DHTTYPE);
+
+
+void setup() {
+  // put your setup code here, to run once:
+  // Start the Serial Monitor at 9600 baud rate
+  Serial.begin(9600);
+
+  // Start the sensor
+  dht.begin();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // Wait a few seconds between measurements (DHT11 is a slow sensor)
+  delay(2000);
+
+  // Read humiditywith decimals
+  float humidity = dht.readHumidity();
+  
+  // Read temperature as Celsius
+  float tempC = dht.readTemperature();
+  
+  // Read temperature as Fahrenheit
+  float tempF = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again)
+  if (isnan(humidity) || isnan(tempC) || isnan(tempF)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+ // Print the results as a JSON string
+  Serial.print("{\"humidity\": ");
+  Serial.print(humidity);
+  Serial.print(", \"temperature\": ");
+  Serial.print(tempC);
+  Serial.println("}");
+
+} 
