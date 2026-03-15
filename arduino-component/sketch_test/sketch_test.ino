427f0d401ba052a8a091e44c0213be1b2d8fdc15
Makker <167777524+MarkusYESSIR@users.noreply.github.com>
2026-03-15 16:43:26 +0100

    Remove temperature in Fahrenheit from sensor readings
    Removed Fahrenheit temperature reading from DHT sensor.

--- arduino-component/sketch_test/sketch_test.ino
+++ arduino-component/sketch_test/sketch_test.ino
@@ -30,12 +30,9 @@ void loop() {
   
   // Read temperature as Celsius
   float tempC = dht.readTemperature();
-  
-  // Read temperature as Fahrenheit
-  float tempF = dht.readTemperature(true);
 
   // Check if any reads failed and exit early (to try again)
-  if (isnan(humidity) || isnan(tempC) || isnan(tempF)) {
+  if (isnan(humidity) || isnan(tempC) {
     Serial.println("Failed to read from DHT sensor!");
     return;
   }
